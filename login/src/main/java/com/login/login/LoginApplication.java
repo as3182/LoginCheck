2e8f932bc8b65b66ca36ddf69243976412ddb41a
ayush sharma <ayush sharma@ayush.mshome.net>
2023-08-29 00:51:19 +0530

    new edit

--- login/src/main/java/com/login/login/LoginApplication.java
+++ login/src/main/java/com/login/login/LoginApplication.java
@@ -2,8 +2,11 @@ package com.login.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.cors.CorsConfiguration;
 
 @SpringBootApplication
+@Import(CorsConfiguration.class)
 public class LoginApplication {
 
 	public static void main(String[] args) {
