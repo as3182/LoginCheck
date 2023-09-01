172a5ca8fd1d83ddd067b93a659c7044ad1d3632
Ayush <Ayush@DESKTOP-PBLU24B>
2023-09-01 10:01:08 +0530

    CorsUpdate

--- login/src/main/java/com/login/login/controller/LoginController.java
+++ login/src/main/java/com/login/login/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.login.login.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import com.login.login.entity.LoginInfo;
 import com.login.login.loginService.LoginInterface;
 
 @RestController
+@CrossOrigin(origins="*")
 public class LoginController {
 	
 	
