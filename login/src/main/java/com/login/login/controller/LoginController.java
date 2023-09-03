84916b527d10c6cf3c2db3d56eb6958d9c1c67f9
Ayush <Ayush@DESKTOP-PBLU24B>
2023-09-03 11:00:53 +0530

    Added Request Mapping

--- login/src/main/java/com/login/login/controller/LoginController.java
+++ login/src/main/java/com/login/login/controller/LoginController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.login.login.entity.LoginInfo;
 import com.login.login.loginService.LoginInterface;
 
 @RestController
+@RequestMapping("/login")
 @CrossOrigin(origins="*")
 public class LoginController {
 	
